b8590e2f0f27e972a8bd9ddda785c71902e03594
kietng04 <124132816+kietng04@users.noreply.github.com>
2025-09-02 01:17:20 +0700

    Add allowedHosts configuration for preview in Vite

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['synergieglobaltest-fe-production.up.railway.app'],
+  },
 })
